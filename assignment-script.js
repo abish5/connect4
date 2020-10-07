c2f6e40451ba3fca597e085ab67c824ccda749bd
seanchung2001 <72354991+seanchung2001@users.noreply.github.com>
2020-10-07 14:01:34 -0400

    Create assignment-script.js

---
+++ assignment-script.js
@@ -0,0 +1,132 @@
+
+function highlightOverHome(){
+    let home = document.getElementById("home-home");
+    home.style.backgroundColor = "black";
+}
+
+function unhighlightHome(){
+    let home = document.getElementById("home-home");
+    home.style.backgroundColor = "red";
+}
+
+function highlightOverGame(){
+    let game = document.getElementById("home-game");
+    game.style.backgroundColor = "black";
+}
+
+function unhighlightGame(){
+    let game = document.getElementById("home-game");
+    game.style.backgroundColor = "red";
+    let gameHome = document.getElementById("game-home");
+    gameHome.style.backgroundColor = "red";
+}
+
+
+function highlightOverLeaderboard(){
+    let leaderboard = document.getElementById("home-leaderboard");
+    leaderboard.style.backgroundColor = "black";
+    let gameLeaderboard = document.getElementById("game-leaderboard");
+    gameLeaderboard.style.backgroundColor = "black";
+}
+
+function unhighlightLeaderboard(){
+    let leaderboard = document.getElementById("home-leaderboard");
+    leaderboard.style.backgroundColor = "red";
+    let gameLeaderboard = document.getElementById("game-leaderboard");
+    gameLeaderboard.style.backgroundColor = "red";
+}
+
+
+function highlightOverInstructions(){
+    let instructions = document.getElementById("home-instructions");
+    instructions.style.backgroundColor = "black";
+    let gameInstructions = document.getElementById("game-instructions");
+    gameLeaderboard.style.backgroundColor = "black";
+}
+
+function unhighlightInstructions(){
+    let instructions = document.getElementById("home-instructions");
+    instructions.style.backgroundColor = "red";
+    let gameInstructions = document.getElementById("game-instructions");
+    gameLeaderboard.style.backgroundColor = "red";
+}
+
+function highlightOverImage(){
+    let image = document.getElementById("profilePicture");
+    image.style.backgroundColor = "red";
+}
+
+function unhighlightImage(){
+    let image = document.getElementById("profilePicture");
+    image.style.backgroundColor = "black"
+}
+
+function GamehighlightOverHome(){
+    let gameHome = document.getElementById("game-home");
+    gameHome.style.backgroundColor = "black";
+}
+
+function GameunhighlightHome(){
+    let gameHome = document.getElementById("game-home");
+    gameHome.style.backgroundColor = "red";
+}
+
+function GamehighlightOverGame(){
+    let gameGame = document.getElementById("game-game");
+    gameGame.style.backgroundColor = "black";
+}
+function GameunhighlightGame(){
+    let gameGame = document.getElementById("game-game");
+    gameGame.style.backgroundColor = "red";
+}
+function GamehighlightOverLeaderboard(){
+    let gameLeaderboard = document.getElementById("game-leaderboard");
+    gameLeaderboard.style.backgroundColor = "black";
+}
+
+function GameunhighlightLeaderboard(){
+    let gameLeaderboard = document.getElementById("game-leaderboard");
+    gameLeaderboard.style.backgroundColor = "red";
+}
+function GamehighlightOverInstructions(){
+    let gameInstructions = document.getElementById("game-instructions");
+    gameInstructions.style.backgroundColor = "black";
+}
+function GameunhighlightInstructions(){
+    let gameInstructions = document.getElementById("game-instructions");
+    gameInstructions.style.backgroundColor = "red";
+}
+
+function LeaderhighlightOverHome(){
+    let leaderHome = document.getElementById("leader-home");
+    leaderHome.style.backgroundColor = "black";
+}
+
+function LeaderunhighlightHome(){
+    let leaderHome = document.getElementById("leader-home");
+    leaderHome.style.backgroundColor = "red";
+}
+function LeaderhighlightOverGame(){
+    let leaderGame = document.getElementById("leader-game");
+    leaderGame.style.backgroundColor = "black";
+}
+function LeaderunhighlightGame(){
+    let leaderGame = document.getElementById("leader-game");
+    leaderGame.style.backgroundColor = "red";
+}
+function LeaderhighlightOverLeaderboard(){
+    let leaderLeader = document.getElementById("leader-leader");
+    leaderLeader.style.backgroundColor = "black";
+}
+function LeaderunhighlightGame(){
+    let leaderLeader = document.getElementById("leader-leader");
+    leaderLeader.style.backgroundColor = "red";
+}
+function LeaderhighlightOverInstructions(){
+    let leaderInstructions = document.getElementById("leader-instructions");
+    leaderLeader.style.backgroundColor = "black";
+}
+function LeaderunhighlightInstructions(){
+    let leaderLeader = document.getElementById("leader-instructions");
+    leaderLeader.style.backgroundColor = "red";
+}

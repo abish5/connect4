30a5771d2f1aa31e4c1acc3e43a3c8e682816e8a
Adam Bishow <39249320+abish5@users.noreply.github.com>
2020-10-22 14:00:59 -0400

    Update client-script.js

--- client-script.js
+++ client-script.js
@@ -1,47 +1,211 @@
 const http = require('http');
 const fs = require('fs');
+const port = 3000;
 
-const server = http.createServer(function(request, response) {
-    fs.readFile('./home-page.html', function (err, html) {
-        if (err) throw err; 
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    });
-    if(request.method ===  'GET'){
-        if(request.url === "./game-page.html"){
-            fs.readFile("game-page.html", function(err, data){
-                if(err){
-                    response.statusCode = 500;
-                    response.end("Your file could not be read.");
-                }else{
-                    console.log("hello");
-                    response.statusCode  = 200;
-                    response.setHeader("Content-Type", "text/html");
-                    response.write(data);
-                    response.end(data);
-                }
+
+const server = http.createServer(function(req, res) {
+    if(req.method === 'GET') {
+        var q = require('url').parse(req.url, true);
+        var filename = "." + q.pathname;
+        console.log(q);
+        console.log(filename);  
+        var contentType = req.headers['content-type'];   
+        //SERVER STARTING REQUESTS
+        if(filename === './' || filename == "./home-page.html"){
+			fs.readFile("./home-page.html", function(err, data){
+				if(err){
+					res.statusCode = 500;
+					res.write("Server error.");
+					res.end();
+					return;
+				}
+				res.statusCode = 200;
+				res.setHeader("Content-Type", "text/html");
+				res.write(data);
+				res.end();
             });
-        }else if(request.url === "./assignment-script.js"){
-            fs.readFile("assignment-script.js", function(err,data){
-                if(err){
-                    response.statusCode = 500;
-                    response.write("Your file could not be read.");
-                    response.end();
-                }
-                else{
-                    response.statusCode = 200;
-                    response.setHeader("Content-Type", "application/javascript");
-                    response.write(data);
-                    response.end();
-                }
+        } 
+        else if(filename === './game-page.html'){
+			fs.readFile(filename, function(err, data){
+				if(err){
+					res.statusCode = 500;
+					res.write("Server error.");
+					res.end();
+					return;
+				}
+				res.statusCode = 200;
+				res.setHeader("Content-Type", "text/html");
+				res.write(data);
+				res.end();
+            });
+		} 
+		else if(filename === './leaderboard.html'){
+			fs.readFile(filename, function(err, data){
+				if(err){
+					res.statusCode = 500;
+					res.write("Server error.");
+					res.end();
+					return;
+				}
+				res.statusCode = 200;
+				res.setHeader("Content-Type", "text/html");
+				res.write(data);
+				res.end();
+            });
+		} 
+		else if(filename === './sign-in.html'){
+			fs.readFile(filename, function(err, data){
+				if(err){
+					res.statusCode = 500;
+					res.write("Server error.");
+					res.end();
+					return;
+				}
+				res.statusCode = 200;
+				res.setHeader("Content-Type", "text/html");
+				res.write(data);
+				res.end();
+            });
+		} 
+		else if(filename === './user-profile.html'){
+			fs.readFile(filename, function(err, data){
+				if(err){
+					res.statusCode = 500;
+					res.write("Server error.");
+					res.end();
+					return;
+				}
+				res.statusCode = 200;
+				res.setHeader("Content-Type", "text/html");
+				res.write(data);
+				res.end();
+            });
+		} 
+		else if(filename === "./js/game.js"){
+			fs.readFile(filename, function(err, data){
+				if(err){
+					res.statusCode = 500;
+					res.write("Server error.");
+					res.end();
+					return;
+				}
+				res.statusCode = 200;
+				res.setHeader("Content-Type", "application/javascript");
+				res.write(data);
+				res.end();
+            });
+		}
+        else if(filename === "./js/script.js"){
+			fs.readFile(filename, function(err, data){
+				if(err){
+					res.statusCode = 500;
+					res.write("Server error.");
+					res.end();
+					return;
+				}
+				res.statusCode = 200;
+				res.setHeader("Content-Type", "application/javascript");
+				res.write(data);
+				res.end();
+            });
+		}
+		else if(filename === "./css/style.css"){
+			fs.readFile(filename, function(err, data){
+				if(err){
+					res.statusCode = 500;
+					res.write("Server error.");
+					res.end();
+					return;
+				}
+				res.statusCode = 200;
+				res.setHeader("Content-Type", "text/css");
+				res.write(data);
+				res.end();
+            });
+        } 
+        else if(filename === "./css/home-page-style.css"){
+			fs.readFile(filename, function(err, data){
+				if(err){
+					res.statusCode = 500;
+					res.write("Server error.");
+					res.end();
+					return;
+				}
+				res.statusCode = 200;
+				res.setHeader("Content-Type", "text/css");
+				res.write(data);
+				res.end();
             });
         }
+        else if(filename === "./img/connect4logo.png"){
+			fs.readFile(filename, function(err, data){
+				if(err){
+					res.statusCode = 500;
+					res.write("Server error.");
+					res.end();
+					return;
+				}
+				res.statusCode = 200;
+				res.setHeader("Content-Type", "image/png");
+				res.write(data);
+				res.end();
+            });
+        }
+        else if(filename === "./img/placeholder-user.png"){
+			fs.readFile(filename, function(err, data){
+				if(err){
+					res.statusCode = 500;
+					res.write("Server error.");
+					res.end();
+					return;
+				}
+				res.statusCode = 200;
+				res.setHeader("Content-Type", "image/png");
+				res.write(data);
+				res.end();
+            });
+		}
+		else if(filename === "./img/yellow-arrow.png"){
+			fs.readFile(filename, function(err, data){
+				if(err){
+					res.statusCode = 500;
+					res.write("Server error.");
+					res.end();
+					return;
+				}
+				res.statusCode = 200;
+				res.setHeader("Content-Type", "image/png");
+				res.write(data);
+				res.end();
+            });
+		}
+		else if(filename === "./img/Connect4Board.png"){
+			fs.readFile(filename, function(err, data){
+				if(err){
+					res.statusCode = 500;
+					res.write("Server error.");
+					res.end();
+					return;
+				}
+				res.statusCode = 200;
+				res.setHeader("Content-Type", "image/png");
+				res.write(data);
+				res.end();
+            });
+        }
+        
     }
     else{
-        response.statusCode = 404;
-        response.end("File not found error.");
+        res.statusCode = 404;
+        res.end("File not found error.");
     }
 })
 
-server.listen(3000);
+server.listen(port, function(error) {
+	if (error) {
+		console.log('Something went wrong', error) 
+	} else {
+        console.log('Server is listenting on port ' + port)
+    }
+});
+console.log('Server running at http://127.0.0.1:3000/');

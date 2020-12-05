9a073800a3e722502492a62f5a6df520823c87e2
seanchung2001 <72354991+seanchung2001@users.noreply.github.com>
2020-12-04 19:53:56 -0500

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -15,7 +15,21 @@ const UserSchema = mongoose.Schema({ // Need to add id and stats?
     },
     friends: {      //Should make this friends ID
         type: [String]
-    }
+    },
+    friendRequests: {
+        type: [String]
+    },
+    public: {
+        type: Boolean
+    },
+    gamesPlayed: {
+        type: Number
+    },
+    winRate: {
+        type: Number
+    },
+    currentGames: [{type: mongoose.Schema.Types.ObjectId, ref: "Game"}]
+    
 }, { collection: "users"});
 
 module.exports = mongoose.model('User', UserSchema);

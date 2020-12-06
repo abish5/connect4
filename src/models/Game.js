ca1e6e534afe9e4cfa0e7d3598cdf0181d6b87f0
seanchung2001 <72354991+seanchung2001@users.noreply.github.com>
2020-12-06 18:49:54 -0500

    Update Game.js

--- src/models/Game.js
+++ src/models/Game.js
@@ -1,21 +1,23 @@
-import mongoose from "mongoose";
-
 const mongoose = require('mongoose');
 
 const GameSchema = mongoose.Schema({ // Need to add id and stats?
-    player: {
-        type: String,
-        required: true
-    },
+    players: 
+        [{type: mongoose.Schema.Types.ObjectId, ref: "User"}],
     active: {
         type: Boolean,
         required: true
     },
     detail: {
         type: String,
-        required: true
     },
+    board: {
+        type: [[Number]]
+    },
+    winner: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
+    turn: {
+        type: Number
+    }
 
 }, { collection: "games"});
 
-module.exports = mongoose.model('User', GameSchema);
+module.exports = mongoose.model('Game', GameSchema);
